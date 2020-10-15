2c4a6a89cd8da388372f03b58657e6e0cf73883b
Mirkamal <37473943+ShouLie@users.noreply.github.com>
2020-10-15 22:48:39 +0400

    Initial commit

---
+++ app/src/main/java/com/mirkamal/gamewatch/ui/activities/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mirkamal.gamewatch.ui.activities.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mirkamal.gamewatch.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/mirkamal/gamewatch/ui/fragments/intro/IntroFragment.kt
@@ -0,0 +1,21 @@
+package com.mirkamal.gamewatch.ui.fragments.intro
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Mirkamal on 15 October 2020
+ */
+class IntroFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
