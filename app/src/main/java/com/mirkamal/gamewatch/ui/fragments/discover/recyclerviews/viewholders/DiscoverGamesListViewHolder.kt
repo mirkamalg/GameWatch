15479c93872f851aca1133b9b26202caafecb3b5
Mirkamal <37473943+ShouLie@users.noreply.github.com>
2020-10-20 15:36:06 +0400

    Added platform icons into discover list

--- app/src/main/java/com/mirkamal/gamewatch/ui/fragments/discover/recyclerviews/viewholders/DiscoverGamesListViewHolder.kt
+++ app/src/main/java/com/mirkamal/gamewatch/ui/fragments/discover/recyclerviews/viewholders/DiscoverGamesListViewHolder.kt
@@ -1,8 +1,13 @@
 package com.mirkamal.gamewatch.ui.fragments.discover.recyclerviews.viewholders
 
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.mirkamal.gamewatch.databinding.ItemDiscoverGamesBinding
 import com.mirkamal.gamewatch.model.entity.Game
+import com.mirkamal.gamewatch.utils.NINTENDO_SWITCH_ID
+import com.mirkamal.gamewatch.utils.PC_ID
+import com.mirkamal.gamewatch.utils.PLAYSTATION_IDs
+import com.mirkamal.gamewatch.utils.XBOX_IDs
 
 /**
  * Created by Mirkamal on 18 October 2020
@@ -18,6 +23,17 @@ class DiscoverGamesListViewHolder private constructor(private val binding: ItemD
 
         //Make textview selected for marquee
         binding.textViewGameName.isSelected = true
+
+        configurePlatformIcons(game)
+    }
+
+    private fun configurePlatformIcons(game: Game) {
+        for (id in game.platforms) {
+            if (id == PC_ID.toLong()) binding.imageViewPC.isVisible = true
+            if (XBOX_IDs.contains(id.toInt())) binding.imageViewXBOX.isVisible = true
+            if (PLAYSTATION_IDs.contains(id.toInt())) binding.imageViewPS.isVisible = true
+            if (id == NINTENDO_SWITCH_ID.toLong()) binding.imageViewNintendo.isVisible = true
+        }
     }
 
     companion object {
