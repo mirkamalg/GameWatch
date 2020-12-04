e0f2325f5aebba27b90207174c90ca37c6944d1c
Mirkamal <37473943+ShouLie@users.noreply.github.com>
2020-12-04 16:43:56 +0400

    Added share as image and text feature

--- app/src/main/java/com/mirkamal/gamewatch/ui/fragments/dialogs/BottomSheetShareGameDetails.kt
+++ app/src/main/java/com/mirkamal/gamewatch/ui/fragments/dialogs/BottomSheetShareGameDetails.kt
@@ -1,16 +1,26 @@
 package com.mirkamal.gamewatch.ui.fragments.dialogs
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.mirkamal.gamewatch.R
+import com.mirkamal.gamewatch.utils.libs.MessageGenerator
+import kotlinx.android.synthetic.main.dialog_share_game_details.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * Created by Mirkamal on 23 November 2020
  */
 class BottomSheetShareGameDetails : BottomSheetDialogFragment() {
+
+    private val args: BottomSheetShareGameDetailsArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +33,28 @@ class BottomSheetShareGameDetails : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        cardViewContainerText.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Check this game out!")
+            intent.putExtra(Intent.EXTRA_TEXT, MessageGenerator.generateShareGameText(args.game))
+            startActivity(Intent.createChooser(intent, "Share using"))
+            dismiss()
+        }
 
+        cardViewContainerImage.setOnClickListener {
+            context?.let { c ->
+                viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+                    val bitmap = Glide.with(c).asBitmap()
+                        .load(args.game.coverURL.replace("t_screenshot_med", "t_screenshot_huge"))
+                        .submit().get()
+                    val intent = Intent(Intent.ACTION_SEND)
+                    intent.type = "image/*"
+                    intent.putExtra(Intent.EXTRA_STREAM, MessageGenerator.getImageUri(c, bitmap))
+                    startActivity(Intent.createChooser(intent, "Share using"))
+                    dismiss()
+                }
+            }
+        }
     }
 }
