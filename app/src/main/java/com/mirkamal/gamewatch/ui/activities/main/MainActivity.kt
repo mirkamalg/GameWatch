c0a3e4a1dcc78b7f525cb881006b26871efe68a9
Mirkamal <37473943+ShouLie@users.noreply.github.com>
2020-12-04 14:16:41 +0400

    Changes status bar background and icon colors

--- app/src/main/java/com/mirkamal/gamewatch/ui/activities/main/MainActivity.kt
+++ app/src/main/java/com/mirkamal/gamewatch/ui/activities/main/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.mirkamal.gamewatch.ui.activities.main
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.mirkamal.gamewatch.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+        }
     }
 }
