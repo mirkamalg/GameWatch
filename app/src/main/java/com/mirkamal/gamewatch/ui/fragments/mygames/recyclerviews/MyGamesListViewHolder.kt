afcdfbfab6da1a7326e4a90fd3ee80a7468736dc
Mirkamal <37473943+ShouLie@users.noreply.github.com>
2020-12-04 11:55:17 +0400

    Added listener for delete button in my games

--- app/src/main/java/com/mirkamal/gamewatch/ui/fragments/mygames/recyclerviews/MyGamesListViewHolder.kt
+++ app/src/main/java/com/mirkamal/gamewatch/ui/fragments/mygames/recyclerviews/MyGamesListViewHolder.kt
@@ -7,11 +7,18 @@ import com.mirkamal.gamewatch.model.entity.Game
 /**
  * Created by Mirkamal on 24 October 2020
  */
-class MyGamesListViewHolder private constructor(private val binding: ItemMyGamesBinding, private val listener: (game: Game) -> Unit) : RecyclerView.ViewHolder(binding.root) {
+class MyGamesListViewHolder private constructor(
+    private val binding: ItemMyGamesBinding,
+    private val listener: (game: Game) -> Unit,
+    private val deleteListener: (position: Int, id: Long) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(game: Game) {
         binding.apply {
             this.game = game
+            this.imageViewDelete.setOnClickListener {
+                deleteListener(adapterPosition, game.id)
+            }
             executePendingBindings()
         }
         itemView.setOnClickListener {
@@ -20,8 +27,12 @@ class MyGamesListViewHolder private constructor(private val binding: ItemMyGames
     }
 
     companion object {
-        fun from(binding: ItemMyGamesBinding, listener: (game: Game) -> Unit) : MyGamesListViewHolder {
-            return MyGamesListViewHolder(binding, listener)
+        fun from(
+            binding: ItemMyGamesBinding,
+            listener: (game: Game) -> Unit,
+            deleteListener: (position: Int, id: Long) -> Unit
+        ): MyGamesListViewHolder {
+            return MyGamesListViewHolder(binding, listener, deleteListener)
         }
     }
 }
