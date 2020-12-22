98967a5a38cf2d7d7c2828d14f5dc9acfaf3db27
Mirkamal <37473943+ShouLie@users.noreply.github.com>
2020-12-22 12:46:05 +0400

    Building profile screen

--- app/src/main/java/com/mirkamal/gamewatch/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/mirkamal/gamewatch/ui/fragments/profile/ProfileFragment.kt
@@ -1,16 +1,30 @@
 package com.mirkamal.gamewatch.ui.fragments.profile
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.mirkamal.gamewatch.R
+import com.mirkamal.gamewatch.utils.BIO_KEY
+import com.mirkamal.gamewatch.utils.USERNAME_KEY
+import com.mirkamal.gamewatch.utils.USER_DATA_COLLECTION_KEY
+import jp.wasabeef.blurry.Blurry
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 /**
  * Created by Mirkamal on 17 October 2020
  */
-class ProfileFragment : Fragment(){
+class ProfileFragment : Fragment() {
+
+    private val db = Firebase.firestore
+    private val email = Firebase.auth.currentUser?.email ?: ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,5 +36,38 @@ class ProfileFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        configureTexts()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        fetchUserData()
+    }
+
+    private fun fetchUserData() {
+        db.collection(USER_DATA_COLLECTION_KEY).document(email).get().addOnSuccessListener {
+            textViewUsername.text = it[USERNAME_KEY] as String
+            textViewBio.text = it[BIO_KEY] as String
+        }
+
+        val threeMegabytes = (1024 * 1024 * 3).toLong()
+        val profilePictureReference = Firebase.storage.reference.child("user_pictures/$email/profile.png")
+        profilePictureReference.getBytes(threeMegabytes).addOnSuccessListener {
+            imageViewProfilePicture.setImageBitmap(BitmapFactory.decodeByteArray(it, 0, it.size))
+        }
+
+        val coverImageReference = Firebase.storage.reference.child("user_pictures/$email/cover.png")
+        coverImageReference.getBytes(threeMegabytes).addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeByteArray(it, 0, it.size)
+            Blurry.with(context).animate(1000).async().from(bitmap).into(imageViewProfileCover)
+            viewGradient.isVisible = true
+        }
+
+    }
+
+    private fun configureTexts() {
+        textViewEmail.text = email
     }
 }
