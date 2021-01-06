e7bdf8f97db9838e3bcd9496409918effd93b07e
Mirkamal <37473943+ShouLie@users.noreply.github.com>
2021-01-06 21:00:16 +0400

    Added animation to epic games card in profile

--- app/src/main/java/com/mirkamal/gamewatch/ui/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/mirkamal/gamewatch/ui/fragments/profile/ProfileFragment.kt
@@ -143,12 +143,12 @@ class ProfileFragment : Fragment() {
 
 
                 if (displayNameEpicGames.isNotBlank()) {
-                    textViewEpicGamesDisplayName.text = displayNameEpicGames
-                    linearLayoutEpicGames.setOnClickListener {
-                        epicGames?.get(URL_KEY)?.let { openURL(it) }
-                    }
+                    textViewDisplayNameEpicGames.text = displayNameEpicGames
+//                    linearLayoutEpicGames.setOnClickListener {
+//                        epicGames?.get(URL_KEY)?.let { openURL(it) }
+//                    }
                 } else {
-                    linearLayoutEpicGames.isVisible = false
+                    cardViewEpicGamesProfile.isVisible = false
                 }
 
                 val uplay = documentSnapshot[UPLAY_KEY] as Map<String, String>?
